6d1d7644a56be78110f0ef47c0dab2e3c6d358ee
LeeYeJin <dldpwls0626@gmail.com>
2024-05-31 10:36:35 +0900

    텍스트마이닝/ streamlit 실습

---
+++ streamlit/streamlit_example.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("나의 첫번째 streamlit 앱")
+# 앱에 제목 생성
+
+st.header('MULCAM 생성형 AI MASTER CLASS에 오신 것을 환영합니다. ')
+st.subheader("웹 앱을 만들기 위한 강력하고 사용하기 쉬운 streamlit 수업")
+# 앱에 header, subheader 생성
+
+st.text('일반 텍스트 입력')
+# 앱에 텍스트나 데이터 생성
+
+st.write("write() 함수 사용, 텍스트 데이터 플롯 표시 가능")
+# 텍스트, 데이터프레임, 플롯 표시
